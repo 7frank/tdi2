0a2d0a71b95eede8147132b605aef5f3d1802c6f
Frank <frank.reimann@jambit.de>
2025-08-13 12:02:29 +0200

    add behaviour testing and rename files

--- monorepo/packages/di-testing/happydom.ts
+++ monorepo/packages/di-testing/happydom.ts
@@ -1,4 +1,5 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
+import "@testing-library/jest-dom";
 
 // Register Happy DOM globals for test environment
 GlobalRegistrator.register();
